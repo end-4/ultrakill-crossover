cc4e7ccd1a43af38f4cb213868f6ea931e54302e
end-4 <97237370+end-4@users.noreply.github.com>
2026-05-20 00:47:19 +0200

    create cross on death instead of on spawn

--- Patches/EnemyPatch.cs
+++ Patches/EnemyPatch.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 namespace Crossover.Patches;
 
 [HarmonyPatch(typeof(EnemyIdentifier))]
-public class EnemyPatch {
+class EnemyPatch {
     [HarmonyPostfix]
-    [HarmonyPatch("Start")]
-    public static void EnemySpawned(EnemyIdentifier __instance) {
-        if (!ConfigManager.Enable.value || __instance.health < ConfigManager.EnemyHealthThreshold.value) return;
+    [HarmonyPatch("ProcessDeath")]
+    static void Postfix(EnemyIdentifier __instance) {
         GameObject cross = Object.Instantiate(Plugin.DeathCrossPrefab, Plugin.DeathCrossCanvas.transform);
-        cross.GetComponent<CanvasGroup>().alpha = 0;
         CrossBehavior crossBehavior = cross.AddComponent<CrossBehavior>();
         crossBehavior.enemy = __instance;
+        cross.SetActive(true);
     }
 }
--- package/CHANGELOG.md
+++ package/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Crossover
 
+## 1.0.1
+
+- Internal: create crosses when enemies die instead of when they spawn. Should help performance a bit.
+
 ## 1.0.0
 
 - Initial release
