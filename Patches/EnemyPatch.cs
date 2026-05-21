79d110f5aa6bc011539df52e27e87bef3f5894f9
end-4 <97237370+end-4@users.noreply.github.com>
2026-05-21 12:33:07 +0200

    respect enable and health threshold config options again

--- Patches/EnemyPatch.cs
+++ Patches/EnemyPatch.cs
@@ -8,6 +8,7 @@ class EnemyPatch {
     [HarmonyPostfix]
     [HarmonyPatch("ProcessDeath")]
     static void Postfix(EnemyIdentifier __instance) {
+        if (!ConfigManager.Enable.value || __instance.health < ConfigManager.EnemyHealthThreshold.value) return;
         GameObject cross = Object.Instantiate(Plugin.DeathCrossPrefab, Plugin.DeathCrossCanvas.transform);
         CrossBehavior crossBehavior = cross.AddComponent<CrossBehavior>();
         crossBehavior.enemy = __instance;
